8ec4f6a2751c19de3098eda74b4c43ad907f127f
Marvin Manguiat <marvinmanguiat@gmail.com>
2014-09-05 14:36:23 +0800

    added dummy files for demo purposes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,15 @@
 $(function() {
 	//alert("DOM ready");
-    
-
+	
+	//auto destruct dissmissable flash message 
+	setTimeout(
+			  function() 
+			  {
+				  $('.alert-dismissable').slideUp(500,function(){
+					  $(this).hide();
+				  })
+				  
+			  }, 5000);
+	
 
 });
