4f327786e604e401dc273783c3b00c71ba61ef88
Marvin Manguiat <marvinmanguiat@gmail.com>
2014-09-09 17:29:36 +0800

    added custom file upload tools

---
+++ vendor/MarvinFileUploadUtils/FileUploadUtils.php
@@ -0,0 +1,45 @@
+<?php 
+/**
+ * Upload file Utility class for common 
+ * File or image  manipulation tools 
+ * @author marvin
+ * 
+ * Sept 9, 2014
+ * Sourcefit Philippines Inc.
+ */
+namespace MarvinFileUploadUtils;
+
+use MarvinFileUploadUtils\UploadClass\Upload;
+
+class FileUploadUtils extends Upload {
+	
+	public $xfile;
+	protected $max_file_size = 2097152; // 2 mb max
+	
+	public function FileUploadUtils($file) {
+		$this->xfile = $file;
+		parent::__construct( $this->xfile );
+	}
+	
+	public  function file_is_image(){
+		return $this->file_is_image;
+	}
+	
+	public function getProcessedFile(){
+		return $this->file_dst_name;
+	}
+	
+	/**
+	 * determine the target file exceeds file limits (2097152) 2 mb
+	 * @return boolean
+	 */
+	public function isFileTooBig(){
+		return ($this->file_src_size > $this->max_file_size);
+	}
+	
+	public function sayHello(){
+		return "Hello from FileUploadUtils Class!";
+	}
+	
+}
+?>
