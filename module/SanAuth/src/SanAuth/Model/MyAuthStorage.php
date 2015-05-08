ede0b12ec53958905b0f81f3ab3b1db49c78c4f3
manguiatmarvin <manguiatmarvin@gmail.com>
2015-05-08 15:19:33 +0800

    Update MyAuthStorage.php

--- module/SanAuth/src/SanAuth/Model/MyAuthStorage.php
+++ module/SanAuth/src/SanAuth/Model/MyAuthStorage.php
@@ -17,4 +17,29 @@ class MyAuthStorage extends Storage\Session
     {
         $this->session->getManager()->forgetMe();
     }
+    
+    /*Set tableGateway as save handler
+    
+    muscreate a table session
+    
+    CREATE TABLE IF NOT EXISTS `session` (
+  `id` char(32) NOT NULL DEFAULT '',
+  `name` char(32) NOT NULL DEFAULT '',
+  `modified` int(11) DEFAULT NULL,
+  `lifetime` int(11) DEFAULT NULL,
+  `data` text,
+  PRIMARY KEY (`id`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    
+    */
+      public function setDbHandler()
+    {
+        $tableGateway = new TableGateway('session', $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter'));
+        $saveHandler = new DbTableGateway($tableGateway, new DbTableGatewayOptions());
+
+        //open session
+        $sessionConfig = new SessionConfig();
+        $saveHandler->open($sessionConfig->getOption('save_path'), $this->namespace);
+        $this->session->getManager()->setSaveHandler($saveHandler);
+    }
 }
